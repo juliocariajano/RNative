f4f251041d7b512e4b0ba6a1424f5abf807ee6cf
jcariajano <juliocariajano@gmail.com>
2022-10-28 16:07:36 -0500

    eliminar favoritos pokemon

--- src/api/favorite.js
+++ src/api/favorite.js
@@ -1,6 +1,5 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { includes, pull } from "lodash";
-import { SafeAreaViewBase } from "react-native";
 import { FAVORITE_STORAGE } from "../utils/constants";
 
 export async function getPokemonsFavorite(){
@@ -22,3 +21,22 @@ try {
     throw error;
 }
 } 
+
+export async function isPokemonFavorite(id){
+    try {
+        const response = await getPokemonsFavorite();
+        return includes(response, id)
+    } catch (error) {
+        
+    }
+}
+
+export async function removePokemonFavorite(id){
+    try {
+        const favorites = await getPokemonsFavorite();
+        const newFavorites = pull(favorites, id)
+        await AsyncStorage.setItem(FAVORITE_STORAGE, JSON.stringify(newFavorites))
+    } catch (error) {
+        throw error
+    }
+}
--- src/components/PokeFavorite.js
+++ src/components/PokeFavorite.js
@@ -1,19 +1,52 @@
-import React from 'react'
-import Icon  from 'react-native-vector-icons/FontAwesome5'
-import { addPokemonFavorite } from '../api/favorite'
+import React, {useState, useEffect} from 'react'
+import { addPokemonFavorite, isPokemonFavorite, removePokemonFavorite} from '../api/favorite'
+
+import FontAwesome5  from 'react-native-vector-icons/FontAwesome5'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 export default function PokeFavorite(props) {
  const {id}= props
+const [isFavorite, setIsFavorite] = useState(undefined)
+const [reloadCheck, setReloadCheck]= useState(false)
+const Icon = isFavorite ? FontAwesome : FontAwesome5;
 
-  const addFavorite =async()=>{
-    addPokemonFavorite(id)
-  }
+  useEffect(()=>{
+    (async ()=>{
+     try {
+        const response = await isPokemonFavorite(id)
+        setIsFavorite(response)      
+     } catch (error) {
+        setIsFavorite(false)
+     }   
+    })();
+    },[id,reloadCheck]);    
 
-  // const  getPokeFav= async()=>{
-  //   const response=  await getPokemonsFavorite();
-  // }
-  return (
-   <Icon name="heart" color="#fff" size={20} onPress={addFavorite} style={{marginRight:20  }}
+    const onReloadCheckFavorite = () =>{
+        setReloadCheck((prev)=> !prev)
+    }
+
+    const addFavorite =async()=>{
+      try {
+        await addPokemonFavorite(id);
+        onReloadCheckFavorite();
+
+      } catch (error) {
+        console.log(error)
+      }
+      }
+    
+    
+  const  removeFavorite = async ()=>{
+    try {
+        await removePokemonFavorite(id);
+        onReloadCheckFavorite()
+    } catch (error) {
+        console.log(error)
+    }    
+}
+ 
+    return (
+   <Icon name="heart" color="#fff" size={20} onPress={isFavorite ? removeFavorite : addFavorite} style={{marginRight:20  }}
 
    />  )
 }
